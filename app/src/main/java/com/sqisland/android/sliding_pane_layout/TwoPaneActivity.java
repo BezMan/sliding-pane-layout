a8ae35ee6da223454fcc8890da20a14b473f16ad
bezalel-s <bezalel-s@yit.co.il>
2016-03-29 09:52:53 +0200

    clickable pane listView

--- app/src/main/java/com/sqisland/android/sliding_pane_layout/TwoPaneActivity.java
+++ app/src/main/java/com/sqisland/android/sliding_pane_layout/TwoPaneActivity.java
@@ -11,18 +11,24 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TwoPaneActivity extends Activity implements View.OnClickListener{
 
-    MySlidingLayout layout;
-    Button btn_toggle, btn_add, btn_sub;
+    MySlidingLayout slidingLayout;
+    Button btn_toggle, btn_add, btn_sub, btn_edit;
     static int count = 0;
     int dimen ;
     LinearLayout layout_pane2;
 
+    ArrayAdapter<String> adapterGrid, adapterList;
     GridView gridView;
+    ListView listView;
 
     static final String[] numbers = new String[] {
             "A", "B", "C", "D", "E",
@@ -31,56 +37,43 @@ public class TwoPaneActivity extends Activity implements View.OnClickListener{
             "P", "Q", "R", "S", "T",
             "U", "V", "W", "X", "Y", "Z"};
 
+    ArrayList<String> listKeys = new ArrayList<>(Arrays.asList(numbers));
+    private boolean isEditable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_pane);
 
-        dimen = getApplicationContext().getResources().getDimensionPixelSize(R.dimen.side_pane_width);
+        initVars();
 
-        btn_toggle = (Button) findViewById(R.id.button_toggle);
-        btn_add = (Button) findViewById(R.id.btn_add);
-        btn_sub = (Button) findViewById(R.id.btn_sub);
-        layout = (MySlidingLayout) findViewById(R.id.sliding_pane_layout);
+        gridView.setAdapter(adapterGrid);
 
-        btn_add.setOnClickListener(this);
-        btn_sub.setOnClickListener(this);
-        btn_toggle.setOnClickListener(this);
-
-        layout_pane2 = (LinearLayout)findViewById(R.id.layout_pane2);
-
-        gridView = (GridView) findViewById(R.id.gridView);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, numbers);
-
-        gridView.setAdapter(adapter);
+        listView.setAdapter(adapterList);
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(getApplicationContext(),
-                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
-            }
-        });
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                String strItem = (String) ((TextView) v).getText();
+                Toast.makeText(getApplicationContext(), strItem, Toast.LENGTH_SHORT).show();
 
-        btn_toggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (layout.isOpen()) {
-                    layout.closePane();
-                } else {
-                    layout.openPane();
+                if (isEditable) {
+                    listKeys.remove(strItem);
+                    gridView.setAdapter(adapterGrid);
                 }
             }
         });
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
 
+            }
+        });
 
-        layout.setSliderFadeColor(Color.TRANSPARENT);
+        slidingLayout.setSliderFadeColor(Color.TRANSPARENT);
 
-        layout.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
+        slidingLayout.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
             @Override
             public void onPanelSlide(View panel, float slideOffset) {
                 Log.wtf("TwoPaneActivity", "onPanelSlide");
@@ -101,6 +94,30 @@ public class TwoPaneActivity extends Activity implements View.OnClickListener{
         });
     }
 
+    private void initVars() {
+        dimen = getApplicationContext().getResources().getDimensionPixelSize(R.dimen.side_pane_width);
+
+        slidingLayout = (MySlidingLayout) findViewById(R.id.sliding_pane_layout);
+        layout_pane2 = (LinearLayout)findViewById(R.id.layout_pane2);
+
+        btn_toggle = (Button) findViewById(R.id.btn_toggle);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        btn_sub = (Button) findViewById(R.id.btn_sub);
+        btn_edit = (Button) findViewById(R.id.btn_edit);
+
+        btn_add.setOnClickListener(this);
+        btn_sub.setOnClickListener(this);
+        btn_toggle.setOnClickListener(this);
+        btn_edit.setOnClickListener(this);
+
+
+        listView = (ListView)findViewById(R.id.listView_pane1);
+        gridView = (GridView) findViewById(R.id.gridView);
+
+        adapterList = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numbers);
+        adapterGrid = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listKeys);
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -112,6 +129,23 @@ public class TwoPaneActivity extends Activity implements View.OnClickListener{
                 count--;
                 Toast.makeText(this, "count == " + count, Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.btn_toggle:
+                if (slidingLayout.isOpen()) {
+                    slidingLayout.closePane();
+                } else {
+                    slidingLayout.openPane();
+                }
+                break;
+            case R.id.btn_edit:
+                if(!isEditable){
+                    btn_edit.setTextColor(Color.GREEN);
+                }
+                else{
+                    btn_edit.setTextColor(Color.BLACK);
+                }
+                isEditable = !isEditable;
+                break;
+
         }
     }
 
